0c40dcbc611f2cd5806745db1587ca2b7cc52905
Júlia Almeida Valadares <julia.valadares@luizalabs.com>
2022-10-11 19:51:19 -0300

    test: adiciona teste builder

---
+++ src/test/java/padroesCriacao/builder/PizzaBuilderTest.java
@@ -0,0 +1,37 @@
+package test.java.padroesCriacao.builder;
+
+import org.junit.Test;
+
+import main.java.padroesCriacao.builder.*;
+import static org.junit.Assert.*;
+
+
+public class PizzaBuilderTest {
+    
+    @Test
+    public void deveRetornarExcecaoParaPizzaSemTamanho() {
+        try {
+            PizzaBuilder PizzaBuilder = new PizzaBuilder();
+            Pizza Pizza = PizzaBuilder
+                    .setTamanho(0)
+                    .build();
+           fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Pizza sem tamanho", e.getMessage());
+        }
+    }
+
+    @Test
+    public void deveRetornarPizzaValido() {
+        PizzaBuilder PizzaBuilder = new PizzaBuilder();
+        Pizza Pizza = PizzaBuilder
+                .setTamanho(30)
+                .setQueijo(true)
+                .setBacon(true)
+                .setBorda(true)
+                .setSaborBorda("Quatro queijos")
+                .build();
+
+        assertNotNull(Pizza);
+    }
+}
